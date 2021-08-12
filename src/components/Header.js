dc30bfd8a4bcc231e1cde5f5849ce7236a7163a1
abhiram11 <abhiramsatpute@gmail.com>
2021-08-12 09:42:29 +0530

    header done, hamburger menu init-ed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
   return (
     <Container>
-      Header
+      {/* Header */}
       <a href="#">
         <img src="/images/logo.svg" alt="Logo" />
       </a>
@@ -17,6 +18,9 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
+        <MenuIconContainer>
+          <MenuIcon />
+        </MenuIconContainer>
       </RightMenu>
     </Container>
   );
@@ -24,7 +28,35 @@ function Header() {
 
 export default Header;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  min-height: 60px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
 
-const MenuGroup = styled.div``;
-const RightMenu = styled.div``;
+const MenuGroup = styled.div`
+  display: flex;
+  text-transform: uppercase;
+  div {
+    font-weight: 600;
+    padding: 0 10px;
+    cursor: pointer;
+  }
+`;
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding-right: 10px;
+  }
+`;
+
+const MenuIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
